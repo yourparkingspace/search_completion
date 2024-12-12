b40686b8561e986d3e7e790fbfaef2e07b15adab
David John Smailes <21348556+dsmailes@users.noreply.github.com>
2024-12-12 13:41:17 +0000

    NEO-2301: correct log

--- ios/Classes/MapKitSearchCompletionManager.swift
+++ ios/Classes/MapKitSearchCompletionManager.swift
@@ -41,7 +41,7 @@ final class MapKitSearchCompletionManager: NSObject, SearchCompletionManagerProt
         super.init()
         
         self.searchCompleter.delegate = self
-        os_log("Search completion manager initialized with region code: %@", log: .default, type: .debug, String(describing: region))
+        os_log("Search completion manager initialized with region: %@", log: .default, type: .debug, String(describing: region))
     }
     
     func returnCoordinatesFromSearchResult(title: String?, subtitle: String?) async -> CLLocationCoordinate2D?  {
